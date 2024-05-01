ac1c33727ad47ea7410c37850bd83d9ee7997a07
Khalil Boudali <boudalikhalil1@gmail.com>
2024-05-01 18:20:21 +0200

    comparing only two books

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,26 @@
 // Main.java
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565");
-        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084");
-        Book book3 = new Book("1984", "George Orwell", "9780451524935");
+        // Create some Book objects
+        Book book1 = new Book("Title 1", "Author 1", "ISBN1");
+        Book book2 = new Book("Title 2", "Author 2", "ISBN2");
+        Book book3 = new Book("Title 3", "Author 3", "ISBN3");
 
-        Library library = new Library(5);
+        // Instantiate the Library object
+        Library library = new Library(5); // Assuming library can hold 5 books
+
+        // Add books to the library
         library.addBook(book1);
         library.addBook(book2);
         library.addBook(book3);
 
-        System.out.println("All books in the library:");
+        // Display all books in the library
         library.showLibraryBooks();
 
+        // Compare between 2 books using the static method
         System.out.println("Comparing books:");
-
-        Book[] allBooks = {book1, book2, book3};
-
-        for (int i = 0; i < allBooks.length; i++) {
-            for (int j = i + 1; j < allBooks.length; j++) {
-                boolean sameISBN = Book.compareBooks(allBooks[i], allBooks[j]);
-                System.out.println(allBooks[i].getTitle() + " and " + allBooks[j].getTitle() + " have same ISBN: " + sameISBN);
-            }
-        }
+        boolean sameIsbn = Book.compareBooks(book1, book2);
+        System.out.println("Are book1 and book2 the same? " + (sameIsbn ? "Yes" : "No"));
     }
 }
 
